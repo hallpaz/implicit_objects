9e170c15345a57e00de2897de3df9c87d433fe30
Hallison Oliveira da Paz <hallpaaz@gmail.com>
2016-03-02 17:41:07 -0300

    compunting nearby voxels

--- marching_cubes.py
+++ marching_cubes.py
@@ -123,16 +123,52 @@ def compute_side(vertex, triangle_vertices):
         return 1
     return -1
 
-def signed_distance(cell, vertices, faces):
+# def signed_distance(cell, vertices, faces):
+#     edges = [
+#         (0, 1), (1, 2), (2, 3), (3, 0),
+#         (4, 5), (5, 6), (6, 7), (7, 4),
+#         (2, 6), (1, 5), (3, 7), (0, 4)
+#     ]
+#     hasintersection = False
+#     for edge in edges:
+#         for indices in faces:
+#             triangle = (vertices[indices.a], vertices[indices.b], vertices[indices.c])
+#             normal = compute_normal(triangle)
+#             intersection_point = triangle_intersection(cell.positions[edge[0]], cell.positions[edge[1]], triangle, normal)
+#             if intersection_point is not None:
+#                 hasintersection = True
+#                 #sign = compute_side(cell.positions[edge[0]], triangle)
+#                 sign = 1 if cell.positions[edge[0]] * normal > 0 else -1
+#                 cell.values[edge[0]] = cell.weights[edge[0]]*cell.values[edge[0]] + sign * Vertex.distance(cell.positions[edge[0]], intersection_point)
+#                 cell.weights[edge[0]] += 1
+#                 cell.values[edge[0]] /= cell.weights[edge[0]]
+#
+#                 cell.values[edge[1]] = cell.weights[edge[1]]*cell.values[edge[1]] -sign * Vertex.distance(cell.positions[edge[1]], intersection_point)
+#                 cell.weights[edge[1]] += 1
+#                 cell.values[edge[1]] /= cell.weights[edge[1]]
+#     return hasintersection
+
+
+def nearby_voxels(vertex):
+    
+
+def signed_distance(grid, vertices, faces):
     edges = [
         (0, 1), (1, 2), (2, 3), (3, 0),
         (4, 5), (5, 6), (6, 7), (7, 4),
         (2, 6), (1, 5), (3, 7), (0, 4)
     ]
     hasintersection = False
-    for edge in edges:
-        for indices in faces:
-            triangle = (vertices[indices.a], vertices[indices.b], vertices[indices.c])
+    flatgrid = []
+    for indices in faces:
+        voxels_indices = []
+        triangle = (vertices[indices.a], vertices[indices.b], vertices[indices.c])
+        voxels_indices.extend(nearby_voxels(triangle[0]))
+        voxels_indices.extend(nearby_voxels(triangle[1]))
+        voxels_indices.extend(nearby_voxels(triangle[2]))
+        voxels_indices = set(voxels_indices)
+        for indices in voxels_indices:
+            cell = grid[indices[0]][indices[1]][indices[2]]
             normal = compute_normal(triangle)
             intersection_point = triangle_intersection(cell.positions[edge[0]], cell.positions[edge[1]], triangle, normal)
             if intersection_point is not None:
@@ -145,19 +181,23 @@ def signed_distance(cell, vertices, faces):
 
                 cell.values[edge[1]] = cell.weights[edge[1]]*cell.values[edge[1]] -sign * Vertex.distance(cell.positions[edge[1]], intersection_point)
                 cell.weights[edge[1]] += 1
-                cell.values[edge[1]] /= cell.weights
-    return hasintersection
+                cell.values[edge[1]] /= cell.weights[edge[1]]
+                flatgrid.append(cell)
+    flatgrid = set(flatgrid)
+    return flatgrid
 
 def assign_distance_to_grid(grid, vertices, faces):
     flatgrid = []
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            for k in range(len(grid[0][0])):
-                cell = grid[i][j][k]
-                if signed_distance(cell, vertices, faces):
-                    flatgrid.append(cell)
-                print(i, j, k)
+    # for i in range(len(grid)):
+    #     for j in range(len(grid[0])):
+    #         for k in range(len(grid[0][0])):
+    #             cell = grid[i][j][k]
+    #             if signed_distance(cell, vertices, faces):
+    #                 flatgrid.append(cell)
+    #             print(i, j, k)
+
     print("all cells with value")
+    return flatgrid
 
 def polygonise_cube(cell, isovalue, vertices, triangles):
     vertlist = 12*[None]
@@ -246,7 +286,7 @@ if __name__ == '__main__':
 
     grid = makeGrid(box_volume, 10)
 
-    grid = assign_distance_to_grid(grid, vertexBuffer, indexBuffer)
+    grid = signed_distance(grid, vertexBuffer, indexBuffer)#assign_distance_to_grid(grid, vertexBuffer, indexBuffer)
 
     vertices = []
     triangles = []
